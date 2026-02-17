baff1950d7b3730ebf12e83168ac8c1b2eefaf9e
Humphrey Manango <humpoxmanango@gmail.com>
2026-02-17 23:46:35 +0800

    I tried to do the Bonus: implemented RecyclerView in Subjects screen

--- Study Budy/app/build.gradle.kts
+++ Study Budy/app/build.gradle.kts
@@ -43,4 +43,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
 }
--- Study Budy/app/src/main/java/com/example/milestone_1/SubjectsActivity.java
+++ Study Budy/app/src/main/java/com/example/milestone_1/SubjectsActivity.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
+import androidx.recyclerview.widget.DividerItemDecoration;
 
 public class SubjectsActivity extends AppCompatActivity {
 
@@ -13,6 +18,28 @@ public class SubjectsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subjects);
 
+        //Set up subject list
+        List<String> subjects = new ArrayList<>();
+        subjects.add("📚 Mathematics");
+        subjects.add("📚 Science");
+        subjects.add("📚 English");
+        subjects.add("📚 Filipino");
+        subjects.add("📚 History");
+        subjects.add("📚 Computer Science");
+        subjects.add("📚 Arts");
+        subjects.add("📚 Music");
+        subjects.add("📚 Physics");
+        subjects.add("📚 P.E");
+
+        //Set up RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.recyclerViewSubjects);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        DividerItemDecoration divider = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(divider);
+        SubjectAdapter adapter = new SubjectAdapter(subjects);
+        recyclerView.setAdapter(adapter);
+
+        //Back button
         Button btnBack = findViewById(R.id.btnBackToDashboard);
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
