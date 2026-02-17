0cdc19c9a050391da1217d254e09befc3f633750
Humphrey Manango <humpoxmanango@gmail.com>
2026-02-17 23:15:40 +0800

    Commit 2 - UI Design: XML layouts for all 5 screens

--- Study Budy/app/src/main/java/com/example/milestone_1/DashboardActivity.java
+++ Study Budy/app/src/main/java/com/example/milestone_1/DashboardActivity.java
@@ -1,24 +1,34 @@
 package com.example.milestone_1;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class DashboardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_dashboard);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button btnGoToSubjects = findViewById(R.id.btnGoToSubjects);
+        btnGoToSubjects.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this, SubjectsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btnGoToProfile = findViewById(R.id.btnGoToProfile);
+        btnGoToProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
--- Study Budy/app/src/main/java/com/example/milestone_1/SplashActivity.java
+++ Study Budy/app/src/main/java/com/example/milestone_1/SplashActivity.java
@@ -1,13 +1,10 @@
 package com.example.milestone_1;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 @SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
@@ -15,12 +12,12 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button btnGetStarted = findViewById(R.id.btnGetStarted);
+        btnGetStarted.setOnClickListener(v -> {
+            Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+            startActivity(intent);
         });
     }
 }
